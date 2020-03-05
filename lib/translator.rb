8c596ae8409fef95f904efb01a07ab9ca82e6556
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 18:23:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,14 @@
 # require modules here
 require "yaml"
 
-def load_library
-  # code goes here
-  
+def load_library(file)
+  emojifile = YAML.load_file(file)
+  hash = {"get_meaning" => {}, "get_emoticon" => {}}
+  emojifile.each do |meaning, emoji| 
+      hash["get_meaning"][emoji[1]] = meaning
+      hash["get_emoticon"][emoji[0]] = emoji[1]
+    end
+  return hash
 end
 
 def get_japanese_emoticon
